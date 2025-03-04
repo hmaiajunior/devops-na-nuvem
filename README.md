7d7472eb1f75503a8d4a8d2f1cfc96328fbd426c
hmaiajunior <juniorce04@gmail.com>
2025-03-04 16:04:45 -0300

    configurando autenticacao Terraform

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # devops-na-nuvem
 Tudo sobre o treinamento de devops na nuvem
+
+
+Links: 
+
+--> Aplicação: https://github.com/kenerry-serain/not-so-simple-ecommerce 
+
+--> Terraform e Ansible: https://github.com/kenerry-serain/not-so-simple-ecommerce-iac
+
+--> Manifestos Kubernets: https://github.com/kenerry-serain/not-so-simple-ecommerce-gitops
---
+++ modulo-03-aws-setup/aula07-AWS-Role-and-Terraform-Authentication/maint.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+  assume_role {
+    role_arn     = var.assume_role
+    session_name = "devops-nuvem"
+    external_id  = var.assume_role.external_id
+  }
+}
