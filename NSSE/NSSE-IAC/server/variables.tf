9cb45a90916a643b1533cd8eb8a58f168a960372
hmaiajunior <juniorce04@gmail.com>
2025-07-15 22:24:37 -0300

    modulo-04-aula-05-create-key-pair-iac

---
+++ NSSE/NSSE-IAC/server/variables.tf
@@ -0,0 +1,42 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "tags" {
+  type = object({
+    Project    = string
+    Enviroment = string
+
+  })
+  default = {
+    Project    = "nsse",
+    Enviroment = "production"
+
+  }
+
+}
+
+variable "assume_role" {
+  type = object({
+    role_arn    = string
+    external_id = string
+
+  })
+
+  default = {
+    role_arn    = "arn:aws:iam::353203138858:role/devopsnanuvem-week"
+    external_id = "97b9dcb3-26e7-4798-830d-79f3dd893374"
+
+  }
+}
+
+variable "ec2_resources" {
+  type = object({
+    key_pair_name = string 
+  })
+
+    default = {
+        key_pair_name = "nsse-produciton-key-pair"
+    }
+}
