8870f02e186267aa54a511050222b68cdddac1ce
hmaiajunior <juniorce04@gmail.com>
2025-07-20 11:34:02 -0300

    modulo-4-aula-11-auto-scaling-group

--- NSSE/NSSE-IAC/server/variables.tf
+++ NSSE/NSSE-IAC/server/variables.tf
@@ -84,3 +84,32 @@ variable "vpc_resources" {
     vpc = "nsse-production-vpc"
   }
 }
+
+variable "control_plane_autoscaling_group" {
+  type = object({
+    name = string
+    max_size = number
+    min_size = number
+    desired_capacity = number
+    health_check_grace_period = number
+    health_check_type = string
+    instance_maintenance_policy = object({
+      min_healthy_percentage = number
+      max_healthy_percentage = number
+    })
+  })
+
+  default = {
+    name = "nsse-production-control-plane-asg"
+    max_size = 1
+    min_size = 1
+    desired_capacity = 1
+    health_check_grace_period = 180
+    health_check_type = "EC2"
+    instance_maintenance_policy = {
+      min_healthy_percentage = 100
+      max_healthy_percentage = 120
+    }
+  }
+  
+}
