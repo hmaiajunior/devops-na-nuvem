0d1f19671f4a262b6bd09187952b86447d42b441
hmaiajunior <juniorce04@gmail.com>
2025-07-24 21:12:52 -0300

    modulo-04-aula-11-terraform-module

--- NSSE/NSSE-IAC/server/modules/ec2/ec2.launch-template.tf
+++ NSSE/NSSE-IAC/server/modules/ec2/ec2.launch-template.tf
@@ -1,12 +1,15 @@
-resource "aws_launch_template" "control_plane" {
+resource "aws_launch_template" "this" {
   name = var.control_plane_launch_template.name
   disable_api_stop        = var.control_plane_launch_template.disable_api_stop
   disable_api_termination = var.control_plane_launch_template.disable_api_termination
   instance_type = var.control_plane_launch_template.instance_type
-  key_name = aws_key_pair.this.key_name
-  image_id = data.aws_ami.this.image_id
+  #key_name = aws_key_pair.this.key_name
+  key_name = var.key_name
+  # image_id = data.aws_ami.this.image_id
+  image_id = var.image_id
   instance_initiated_shutdown_behavior = var.control_plane_launch_template.instance_initiated_shutdown_behavior
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  #vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   block_device_mappings {
     device_name = "/dev/sdf"
@@ -20,7 +23,8 @@ resource "aws_launch_template" "control_plane" {
 
 
   iam_instance_profile {
-    name = "aws_iam_instance_profile.control_plane.name"
+    #name = "aws_iam_instance_profile.control_plane.name"
+    name = var.instance_profile_name
   }
 
   instance_market_options {
